bf03f68262dcbed99a7a862f164589cadcef87f4
Maina.dev <ianmaina499@gmail.com>
2026-02-26 12:13:21 +0300

    updated the controller

--- day-2-laravel-setup-and-routing/blog-api/app/Http/Controllers/Controller.php
+++ day-2-laravel-setup-and-routing/blog-api/app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    use App\Models\Post;
+
+public function index()
+{
+    $posts = Post::all();
+    return view('posts', compact('posts'));
+}
 }
