ee5cec4c1c25e01ea160d17822a711f65f363413
Maina.dev <ianmaina499@gmail.com>
2026-02-27 21:35:37 +0300

    create crud controller

--- day-2-laravel-setup-and-routing/blog-api/routes/web.php
+++ day-2-laravel-setup-and-routing/blog-api/routes/web.php
@@ -2,5 +2,4 @@
 
 use App\Http\Controllers\PostController;
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::resource('posts', PostController::class);
